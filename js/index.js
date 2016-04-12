12c50947de2702c90bef1a44781b64557b98c45a
Mark <515339612@qq.com>
2016-04-12 17:48:58 +0800

    angular.js框架搭建

---
+++ js/index.js
@@ -0,0 +1,137 @@
+$(function (){
+	var $menuLi= $('#menu').find('.menu_li');
+	var $menuLiChild= $('#menu').find('.menu_child');
+
+	$menuLi.click(function (){
+		
+		$('.menu_ul').hide();
+
+		if($(this).next('.menu_ul')){
+
+			$(this).next('.menu_ul').css('display','block');	
+
+		}else{
+
+			$('.menu_ul').hide();
+		
+		}
+
+		
+	});
+	
+	
+	
+	$menuLiChild.click(function (){
+		
+		
+		if($(this).parents('.menu_ul').html()){
+
+			var rightParentName=$(this).parents('li').find('.menu_li').eq(0).html();
+			var rightNameTxt = $(this).html();
+
+			$('.right_name').html(rightParentName+" "+rightNameTxt);
+			$('.span-12').hide();
+
+			if(rightNameTxt=='调用历史'){
+				
+				$('.span-12').show();
+			}else{
+				$('.span-12').hide();
+				
+			}
+			$('.title-2').html(rightNameTxt);
+			
+			
+
+		}else{
+
+			$('.right_name').html('首页');
+			$('.title-2').html('首页');
+		}
+
+	});
+	
+	
+	
+	
+});
+
+
+angular.module('app',[
+    'ngRoute',
+    'welcome',
+	'rishSearch',
+	'riskHistory'
+])
+    .config(['$routeProvider',function($routeProvider){
+        $routeProvider.otherwise({
+            redirectTo:'/welcome'
+        });
+    }]);
+
+
+
+
+
+(function(){
+	angular.module('welcome',['ngRoute'])
+		.controller('welcomePage',function($scope){
+			
+		})
+		
+		.config(['$routeProvider',function($routeProvider){
+			$routeProvider.when('/welcome',{
+                templateUrl:'page/welcome.html',
+                controller:'welcomePage'
+            });
+			
+		}]);
+		
+	angular.module('rishSearch',['ngRoute'])
+		.controller('riskSearchPage',function($scope){
+			
+			
+		})
+		.config(['$routeProvider',function($routeProvider){
+			$routeProvider.when('/riskSearch',{
+                templateUrl:'page/riskSearch.html',
+                controller:'riskSearchPage'
+            });
+				
+		}]);
+		
+		
+	angular.module('riskHistory',['ngRoute'])
+		.controller('riskHistoryPage',function($scope){
+			
+			
+		})
+		.config(['$routeProvider',function($routeProvider){
+			$routeProvider.when('/riskHistory',{
+                templateUrl:'page/riskHistory.html',
+                controller:'riskHistoryPage'
+            });
+				
+		}]);
+	/*	
+	angular.module('userInfo',['ngRoute'])
+		.controller('userInfoPage',function($scope){
+			
+			
+		})
+		.config(['$routeProvider',function($routeProvider){
+			$routeProvider.when('/userInfo',{
+                templateUrl:'page/userInfo.html',
+                controller:'userInfoPage'
+            });
+				
+		}]);
+	*/
+		
+	
+	
+		
+	
+	
+	
+})();
